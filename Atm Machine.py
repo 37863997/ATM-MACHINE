6f0ec94cb856ab663e167ac25fea363872921ad3
Jesse Mugo <145917688+37863997@users.noreply.github.com>
2024-07-06 08:39:11 -0700

    Create Atm Machine.py

---
+++ Atm Machine.py
@@ -0,0 +1,49 @@
+# Creating an ATM Machine
+
+# initial balance
+Balance= 1000.0
+
+def Atm():
+    print("Welcome to the Atm Machine")
+    while true:
+        print("\nPlease choose an option:")
+        print("1.Check balance")
+        print("2.Deposit Money")
+        print("3.Withdraw Money")
+        print("4.Exit")
+ # Enter your choice whether to withdraw,check balance or Deposit
+        Option=int(input("\nEnter your Option:"))
+        if Option ==1:
+            print(f"\nYour current balance is: ${Balance:.1f}")
+# Deposit amount option
+        elif Option ==2:
+            deposit_amount=float(input("\nEnter amount to deposit: $"))
+            if deposit_amount>0:
+                Balance
+                Balance=Balance+deposit_amount
+                print(f"\nYou have successfully Deposited ${deposit_amount:.1f}")
+                print(f"Your new Balance is:${Balance:.1f}")
+            else:
+                print("\nInvalid deposit_amount .Please try again.")
+# Withdrawal amount option
+        elif Option ==3:
+            withdraw_amount=float(input("\nEnter amount to withdraw: $"))
+            if 0 < withdraw_amount<=Balance:
+                Balance=Balance-withdraw_amount
+                print(f"\nYou have successfully withdrwan ${withdraw_amount:.1f}") 
+                print(f"Your new balance is:${Balance:.1f}")
+            else:
+                print("\nInvalid withdrawal amount.Please try again")
+        elif Option == 4:
+            print("\nThank you for using the ATM.Goodbye!")
+            break
+        else:
+            print("\nInvalid choice.Please try again.")
+
+
+
+
+    
+
+
+        
